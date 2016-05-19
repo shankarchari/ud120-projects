4171d2ca83afae8ef1c6e8c02cec454a85f39efb
Gowri Shankar <gowrishankar_m@hotmail.com>
2016-05-19 21:11:03 +0530

    Decision trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -27,5 +27,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
+from sklearn.metrics import accuracy_score
+from sklearn import tree
 
+print len(features_train[0])
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acc = accuracy_score(labels_test, pred)
+print acc
 
